2b37ee36c9e514984770bf316f903ee3cd9fde9e
Malinna Leach <malinnab@yahoo.com>
2016-09-12 13:23:30 +0100

    exercises 8 and 9

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
 def input_students
   students = []
   additional_info = [:cohort, :hobbies, :country_of_birth, :height]
-  valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october,:november, :december]
+  $valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october,:november, :december]
   default_cohort = :november
   while true do
     hash = {cohort: default_cohort}
-    puts "Please enter a student name, or to finish, hit return twice."
+    puts "Please enter a student name, or to finish, hit return."
     name = gets.chomp
     if name.empty?
       break
@@ -21,7 +21,7 @@ def input_students
             puts
             break
           end
-          if valid_cohorts.any? {|x| x == input.to_sym}
+          if $valid_cohorts.any? {|x| x == input.downcase.to_sym}
             hash[info] = input.to_sym
             break
           else
@@ -34,6 +34,9 @@ def input_students
       end
     end
     students << hash
+    print "Now we have #{students.length} student"
+    puts (students.length > 1)? "s." : "."
+    puts
   end
   students
 end
@@ -45,19 +48,18 @@ def print_header
 end
 
 def prints(students)
-  index = 0
   students.each do |student|
     if ((student[:name].split "")[0].downcase == $letter.downcase) || $letter.empty?
       $report << {name: student[:name], cohort: student[:cohort]}
     end
   end
-  until index == $report.length do
-    student = $report[index]
-    if student[:name].length < 12
-    $count += 1
-    puts "#{$count}. #{student[:name].center(12)} (#{student[:cohort]} cohort)"
+  $valid_cohorts.each do |cohort|
+    $report.each do |student|
+      if student[:name].length < 12 && student[:cohort] == cohort
+        $count += 1
+        puts "#{$count}. #{student[:name].center(12)} (#{student[:cohort]} cohort)"
+      end
     end
-    index += 1
   end
 end
 
@@ -66,22 +68,13 @@ def print_footer(names)
   if $report.length > 1
     print "s"
   end
-  if !$letter.empty?
-    puts " whose name begins with #{$letter}."
-  else
-    puts "."
-  end
+  puts (!$letter.empty?)? " whose name begins with #{$letter}." : "."
   if $count < $report.length
     print "#{$report.length - $count} student"
-    if ($report.length - $count) > 1
-      print "s were"
-    else
-      print " was"
-    end
+    print (($report.length - $count) > 1)? "s were" : " was"
     puts " not listed as their name is more than 12 characters."
   end
 end
-#nothing happens until we call the methods
 
 students = input_students
 if students.length > 0
@@ -93,5 +86,5 @@ if students.length > 0
   prints(students)
   print_footer(students)
 else
-  puts "No students have been entered into the system"
+  puts "No students have been entered into the system."
 end
