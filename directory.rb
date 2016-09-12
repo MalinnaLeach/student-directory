82fffdded8d5ad8f45a99c2d916e48c4061ea4e5
Malinna Leach <malinnab@yahoo.com>
2016-09-12 11:54:33 +0100

    specifying cohorts

--- directory.rb
+++ directory.rb
@@ -1,19 +1,37 @@
 def input_students
   students = []
-  additional_info = [:hobbies, :country_of_birth, :height]
+  additional_info = [:cohort, :hobbies, :country_of_birth, :height]
+  valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october,:november, :december]
+  default_cohort = :november
   while true do
-    hash = {}
+    hash = {cohort: default_cohort}
     puts "Please enter a student name, or to finish, hit return twice."
     name = gets.chomp
-      if name.empty?
-        break
-      end
+    if name.empty?
+      break
+    end
     hash[:name] = name
-    hash[:cohort] = :november
     additional_info.each do |info|
       puts "Please specify #{info.to_s} for #{name}."
       input = gets.chomp
-      hash[info] = input
+      if info == :cohort
+        while true do
+          if input.empty?
+            puts "Default cohort of #{default_cohort.to_s} has been input."
+            puts
+            break
+          end
+          if valid_cohorts.any? {|x| x == input.to_sym}
+            hash[info] = input.to_sym
+            break
+          else
+            puts "That is not a valid cohort name.  Please try again, or hit return for the default cohort."
+            input = gets.chomp
+          end
+        end
+      else
+        hash[info] = input
+      end
     end
     students << hash
   end
