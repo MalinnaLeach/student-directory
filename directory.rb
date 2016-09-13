6d7e14f14c729e0d71c6fed6a1823cc42f925e6e
Malinna Leach <malinnab@yahoo.com>
2016-09-13 12:22:40 +0100

    reading from file

--- directory.rb
+++ directory.rb
@@ -4,6 +4,15 @@
 @valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october,:november, :december]
 @letter = ""
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -15,6 +24,8 @@ def interactive_menu
         show_students
       when "3"
         save_students
+      when "4"
+        load_students
       when "9"
         exit
       else
@@ -28,6 +39,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
