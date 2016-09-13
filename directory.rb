d61c5df1aa5605c30cbe98efc91f39df26f6c7a1
Malinna Leach <malinnab@yahoo.com>
2016-09-13 10:38:54 +0100

    adding @students

--- directory.rb
+++ directory.rb
@@ -1,27 +1,35 @@
+@students = []
+@report = []
+@count = 0
+@valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october,:november, :december]
+@letter = ""
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     selection = gets.chomp
     case selection
-    when "1"
-      students = input_students
-    when "2"
-      display_list students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
+      when "1"
+        input_students
+      when "2"
+        show_students
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
     end
   end
 end
 
+def print_menu
+  puts
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
 def input_students
-  students = []
   additional_info = [:cohort, :hobbies, :country_of_birth, :height]
-  $valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october,:november, :december]
   default_cohort = :november
   while true do
     hash = {cohort: default_cohort}
@@ -41,7 +49,7 @@ def input_students
             puts
             break
           end
-          if $valid_cohorts.any? {|x| x == input.downcase.to_sym}
+          if @valid_cohorts.any? {|x| x == input.downcase.to_sym}
             hash[info] = input.downcase.to_sym
             break
           else
@@ -53,12 +61,11 @@ def input_students
         hash[info] = input
       end
     end
-    students << hash
-    print "Now we have #{students.length} student"
-    puts (students.length > 1)? "s." : "."
+    @students << hash
+    print "Now we have #{@students.length} student"
+    puts (@students.length > 1)? "s." : "."
     puts
   end
-  students
 end
 
 def print_header
@@ -67,44 +74,42 @@ def print_header
   puts "-------------"
 end
 
-def prints(students)
-  students.each do |student|
-    if ((student[:name].split "")[0].downcase == $letter.downcase) || $letter.empty?
-      $report << {name: student[:name], cohort: student[:cohort]}
+def print_student_list
+  @students.each do |student|
+    if ((student[:name].split "")[0].downcase == @letter.downcase) || @letter.empty?
+      @report << {name: student[:name], cohort: student[:cohort]}
     end
   end
-  $valid_cohorts.each do |cohort|
-    $report.each do |student|
+  @valid_cohorts.each do |cohort|
+    @report.each do |student|
       if student[:name].length < 12 && student[:cohort] == cohort
-        $count += 1
-        puts "#{$count}. #{student[:name].center(12)} (#{student[:cohort]} cohort)"
+        @count += 1
+        puts "#{@count}. #{student[:name].center(12)} (#{student[:cohort]} cohort)"
       end
     end
   end
 end
 
-def print_footer(names)
-  print "Overall, we have #{$report.length} great student"
-  if $report.length > 1
+def print_footer
+  print "Overall, we have #{@report.length} great student"
+  if @report.length > 1
     print "s"
   end
-  puts (!$letter.empty?)? " whose name begins with #{$letter}." : "."
-  if $count < $report.length
-    print "#{$report.length - $count} student"
-    print (($report.length - $count) > 1)? "s were" : " was"
+  puts (!@letter.empty?)? " whose name begins with #{@letter}." : "."
+  if @count < @report.length
+    print "#{@report.length - @count} student"
+    print ((@report.length - @count) > 1)? "s were" : " was"
     puts " not listed as their name is more than 12 characters."
   end
 end
 
-def display_list students
-  if students.length > 0
+def show_students
+  if @students.length > 0
     puts "Would you like to specify a first letter?  If so, type the letter, otherwise hit return."
-    $letter = gets.delete "\n"
-    $report = []
-    $count = 0
+    @letter = gets.delete "\n"
     print_header
-    prints(students)
-    print_footer(students)
+    print_student_list
+    print_footer
   else
     puts "No students have been entered into the system."
   end
